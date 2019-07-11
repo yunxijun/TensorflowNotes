54d7672a17fc3ec3795774db2b758528a1719df5
asus <2319085206@qq.com>
2019-07-11 15:07:10 +0800

    add breakpoint-continue training

--- minist_backward.py
+++ minist_backward.py
@@ -43,6 +43,10 @@ def backward(mnist):
     with tf.Session() as sess:
         init_op = tf.global_variables_initializer()
         sess.run(init_op)
+        # 实现断点续训
+        ckpt = tf.train.get_checkpoint_state(MODEL_SAVE_PATH)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
 
         for i in range(STEPS):
             xs, ys = mnist.train.next_batch(BATCH_SIZE)
