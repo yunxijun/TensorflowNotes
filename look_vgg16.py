2329c4f53cf4f7ac71df3418cb9a2cbddfa335d4
asus <2319085206@qq.com>
2019-07-11 20:22:00 +0800

    add look-vgg file

---
+++ look_vgg16.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+
+
+class Vgg16():
+    def __init__(self, vgg16_path=None):
+        if vgg16_path is None:
+            vgg16_path = os.path.join(os.getcwd(), "vgg_weight/vgg16.npy")
+            self.data_dict = np.load(vgg16_path, encoding='latin1').item()
+            print(self.data_dict['conv1_1']['weights'])
+        for x in self.data_dict:
+            print(x)
+
+
+def main():
+    return Vgg16()
+
+
+if __name__ == "__main__":
+    main()
